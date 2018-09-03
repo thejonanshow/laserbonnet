00c5faff5f06738c5238bb0f5c32385644b2600a
Jonan Scheffler <jonanscheffler@gmail.com>
2018-09-02 17:30:13 -0700

    Fix mac address conditionals.

--- scripts/mac_address.sh
+++ scripts/mac_address.sh
@@ -1,10 +1,10 @@
 # Find MAC of eth0, or wlan0 if eth0 does not exist.
 
-if [[ -e /sys/class/net/eth0 ]]; then
+if [ -e /sys/class/net/eth0 ]; then
   MAC=$(cat /sys/class/net/eth0/address)
-elif [[ -e /sys/class/net/enx* ]]; then
+elif [ -e /sys/class/net/enx* ]; then
   MAC=$(cat /sys/class/net/enx*/address)
-elif [[ -e /sys/class/net/wlan0 ]]; then
+elif [ -e /sys/class/net/wlan0 ]; then
   MAC=$(cat /sys/class/net/wlan0/address)
 else
   MAC="unknown_mac_address"
