209f8db7b085c6d5e995d62f48cf6c0c2658425e
Jonan Scheffler <jonanscheffler@gmail.com>
2018-09-06 14:27:20 -0700

    Blink service.

--- scripts/install_services.sh
+++ scripts/install_services.sh
@@ -2,10 +2,12 @@
 echo "Stopping services..."
 systemctl stop laserbonnet
 systemctl stop joybonnet
+systemctl stop blink
 
 echo "Copying new service files to systemd..."
 cp /home/pi/src/laserbonnet/services/joybonnet.service /etc/systemd/system/joybonnet.service
 cp /home/pi/src/laserbonnet/services/laserbonnet.service /etc/systemd/system/laserbonnet.service
+cp /home/pi/src/laserbonnet/services/blink.service /etc/systemd/system/blink.service
 
 echo "Reloading services..."
 systemctl daemon-reload
