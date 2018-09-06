c3003a46ffcd6aeb3f92caf9dad46cd2676ed6bc
Jonan Scheffler <jonanscheffler@gmail.com>
2018-09-06 14:17:14 -0700

    Blink pi led when it starts listening.

---
+++ scripts/blink.rb
@@ -0,0 +1,30 @@
+require "date"
+
+@start_secs = DateTime.parse(`date +"%b %d %T"`).to_time.to_i + 10
+
+def blink
+  [1,0,1,0,1,0].each do |n|
+    `echo #{n} | tee /sys/class/leds/led0/brightness`
+    sleep 0.5
+  end
+end
+
+def laserbonnet_ready
+  # journalctl_output = `journalctl -u laserbonnet | tail -n 1`.chomp
+  journalctl_output = "Sep 04 #{DateTime.now.strftime("%T")} raspberrypi bash[313]: Laserbonnet is listening..."
+
+  journal_secs = DateTime.parse(journalctl_output[0..14]).to_time.to_i
+
+  log_line = journalctl_output.split(/bash\[\d+\]: /).last
+  expected_line = "Laserbonnet is listening..."
+
+  puts @start_secs - journal_secs
+  @start_secs <= journal_secs && log_line == expected_line
+end
+
+until (laserbonnet_ready)
+  puts "Waiting for laserbonnet to come online..."
+  sleep 0.5
+end
+
+blink
