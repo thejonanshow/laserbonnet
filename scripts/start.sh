51e40527fab9f70a19cc2fb0715433f61b038e1f
Jonan Scheffler <jonanscheffler@gmail.com>
2018-09-06 18:40:35 -0700

    Confirm network before pulling repo and starting.

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,12 +4,17 @@ chruby 2.5.1
 echo "changed ruby with chruby"
 echo "using $(ruby -v)"
 
-git pull origin master
-bundle install
+wget -q --tries=10 --timeout=20 --spider http://google.com
+if [[ $? -eq 0 ]]; then
+  git pull origin master
+  bundle install
 
-gpg --batch --yes -r Astro -o /home/pi/src/laserbonnet/.env -d /home/pi/src/laserbonnet/.env.enc
-echo "updated .env"
+  gpg --batch --yes -r Astro -o /home/pi/src/laserbonnet/.env -d /home/pi/src/laserbonnet/.env.enc
+  echo "updated .env"
 
-echo "starting laserbonnet"
-ruby /home/pi/src/laserbonnet/start_laserbonnet.rb
-echo "laserbonnet stopped"
+  echo "starting laserbonnet"
+  ruby /home/pi/src/laserbonnet/start_laserbonnet.rb
+  echo "laserbonnet stopped"
+else
+  echo "no network connection"
+fi
