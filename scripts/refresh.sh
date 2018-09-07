25debb299a11ab75dcb5c37ef329065ad188ab8c
Jonan Scheffler <jonanscheffler@gmail.com>
2018-09-06 21:33:55 -0700

    Retry network connectivity test.

--- scripts/refresh.sh
+++ scripts/refresh.sh
@@ -1,4 +1,27 @@
-wget -q --tries=10 --timeout=20 --spider http://google.com
+function fail {
+  echo $1 >&2
+  exit 1
+}
+
+function retry {
+  local n=1
+  local max=5
+  local delay=5
+  while true; do
+    "$@" && break || {
+      if [[ $n -lt $max ]]; then
+        ((n++))
+        echo "Unable to reach Google. Attempt $n/$max:"
+        sleep $delay;
+      else
+        fail "Network test failed after $n attempts."
+      fi
+    }
+  done
+}
+
+retry wget -q --tries=10 --timeout=20 --spider http://google.com
+
 if [[ $? -eq 0 ]]; then
   echo "network is online"
 
@@ -6,6 +29,4 @@ if [[ $? -eq 0 ]]; then
   echo "git pull complete"
 
   /home/pi/src/laserbonnet/scripts/install_services.rb
-else
-  echo "no network connection"
 fi
