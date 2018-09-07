3d5a1fd7e32ccf391dcbab72d54b3217fe5ccbdd
Jonan Scheffler <jonanscheffler@gmail.com>
2018-09-06 19:02:55 -0700

    All services wait for refresh, install services.

---
+++ scripts/refresh.sh
@@ -0,0 +1,15 @@
+wget -q --tries=10 --timeout=20 --spider http://google.com
+if [[ $? -eq 0 ]]; then
+  echo "network is online"
+
+  git pull origin master
+  bundle install
+  echo "git pull complete"
+
+  gpg --batch --yes -r Astro -o /home/pi/src/laserbonnet/.env -d /home/pi/src/laserbonnet/.env.enc
+  echo "updated .env"
+
+  /home/pi/src/laserbonnet/scripts/install_services.sh
+else
+  echo "no network connection"
+fi
