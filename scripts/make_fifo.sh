1efae013f77edf41800f6815988fd38817122b0e
Jonan Scheffler <jonanscheffler@gmail.com>
2018-09-06 19:45:13 -0700

    We're not using named pipes anymore.

--- scripts/make_fifo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [[ -e /home/pi/src/laserbonnet/bonnet_pipe ]]; then
-  echo bonnet_pipe already exists
-else
-  mkfifo /home/pi/src/laserbonnet/bonnet_pipe
-  chown pi:pi /home/pi/src/laserbonnet/bonnet_pipe
-  echo created bonnet_pipe
-fi
